5ed07e0a1b85094536d64956aa2fe75c7210f94d
MdRehanAli <rehanali21d@gmail.com>
2024-05-10 12:38:52 +0600

    Added navbar and footer section

---
+++ src/pages/Home/Home/Home.jsx
@@ -0,0 +1,11 @@
+
+const Home = () => {
+    return (
+        <div>
+            <h1>This is home.</h1>
+            <img src="../../../assets/l.png" alt="" />
+        </div>
+    );
+};
+
+export default Home;
