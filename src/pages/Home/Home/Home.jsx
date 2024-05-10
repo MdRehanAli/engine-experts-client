931f31da421212a24ee7f836e26f022ca20346c3
MdRehanAli <rehanali21d@gmail.com>
2024-05-10 13:12:58 +0600

    Added Carousel

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Banner from "../Banner/Banner";
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
+
             <h1>This is home.</h1>
-            <img src="../../../assets/l.png" alt="" />
         </div>
     );
 };
