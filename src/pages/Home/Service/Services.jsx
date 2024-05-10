25d34a42c0cb225d5340a06c6784cf4cfa1b9bed
MdRehanAli <rehanali21d@gmail.com>
2024-05-10 16:04:17 +0600

    added services

--- src/pages/Home/Service/Services.jsx
+++ src/pages/Home/Service/Services.jsx
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
 
 
 const Services = () => {
+
+    const [services, setServices] = useState([])
+    useEffect(() => {
+        fetch('services.json')
+            .then(res => res.json())
+            .then(data => setServices(data))
+    }, [])
+
     return (
-        <div>
-            <div className="text-center ">
-                <h1 className="text-3xl">Service</h1>
+        <div className="my-16">
+            <div className="text-center space-y-4">
+                <h1 className="text-2xl font-bold text-orange-600">Service</h1>
                 <h1 className="text-5xl">Our Service Area</h1>
-                <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised <br /> words which don not look even slightly believable. </p>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {
+                    services.map(service => <ServiceCard key={service._ic} service = {service}></ServiceCard>)
+                }
             </div>
         </div>
     );
