10726e8a6874aac27f7af864309e2ea142bcf508
MdRehanAli <rehanali21d@gmail.com>
2024-05-10 15:37:11 +0600

    Added Hero Section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,13 @@
+import About from "../About/About";
 import Banner from "../Banner/Banner";
+import Services from "../Service/Services";
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <About></About>
+            <Services></Services>
 
             <h1>This is home.</h1>
         </div>
---
+++ src/pages/Home/Service/Services.jsx
@@ -0,0 +1,15 @@
+
+
+const Services = () => {
+    return (
+        <div>
+            <div className="text-center ">
+                <h1 className="text-3xl">Service</h1>
+                <h1 className="text-5xl">Our Service Area</h1>
+                <p>the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+            </div>
+        </div>
+    );
+};
+
+export default Services;
